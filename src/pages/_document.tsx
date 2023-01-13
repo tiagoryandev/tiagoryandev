8b393b665fd8c82329c3fe4c3b6c51f45c545dc2
tiagoryandev <tiagoryandev@gmail.com>
2023-01-13 19:47:19 -0300

    feat: initalizate project with nextjs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
+import {
+	Html,
+	Head,
+	Main,
+	NextScript
+} from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+const Document = () => {
+	return (
+		<Html lang="pt">
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+};
+
+export default Document;
