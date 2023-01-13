3c9ff92c1972d5acc862691d47a4aa10008442c9
tiagoryandev <tiagoryandev@gmail.com>
2023-01-13 19:54:26 -0300

    feat: add tailwindcss in project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import type { AppProps } from "next/app";
 
+import "../styles/globals.css";
+
 const App = ({ Component, pageProps }: AppProps) => {
 	return <Component {...pageProps} />;
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
 	return (
-		<h1>Hello, Word!</h1>
+		<h1 className="text-xl text-center font-bold">Hello, Word!</h1>
 	);
 };
 
