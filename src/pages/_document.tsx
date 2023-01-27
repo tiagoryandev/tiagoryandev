57d7e088a01e5e7a9b27d21ed9be48fec22a8164
tiagoryandev <tiagoryandev@gmail.com>
2023-01-27 17:50:26 -0300

    chore: remove google search meta tag verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,6 @@ const Document = () => {
 			<Head>
 				<meta name="description" content="Portfólio de Programador do Tiago Ryan." />
 				<meta name="keywords" content="Programador,BackEnd,JavaScript,React.js,Node.js,Portfolio" />
-				<meta name="google-site-verification" content="hHS5ewQ2F9nhmoltyJwvXYykswtBuKUs1ud4YQd88tg" />
 				<meta name="robots" content="index,follow"></meta>
 			</Head>
 			<body>
