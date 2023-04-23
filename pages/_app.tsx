b804b1dc7986a8fd7404071ca3eb4e9c8b0fc1bf
tiagoryandev <tiagoryandev@gmail.com>
2023-04-23 18:08:58 -0300

    style: use my eslint pattern

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import "@/styles/globals.css";
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+	return <Component {...pageProps} />;
+};
+
+export default App;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+const Document: React.FC = () => {
+	return (
+		<Html lang="en">
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+};
+
+export default Document;
