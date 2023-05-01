649c6e6715425a9eb487ba046e1c9cb9f3f02ab6
tiagoryandev <tiagoryandev@gmail.com>
2023-05-01 12:06:31 -0300

    feat: add content in home page

--- components/WindowContent.tsx
+++ components/WindowContent.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 type Props = {
 	children: React.ReactNode;
 };
 
 const WindowContent: React.FC<Props> = ({ children }) => {
+	const router = useRouter();
+	const isOpen = ["/projects", "/jobs", "/source-code"].includes(router.route);
+
 	return (
-		<div className="col-span-1">{children}</div>
+		<div className={`${isOpen ? "col-span-1" : "col-span-2"}`}>{children}</div>
 	);
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Image from "next/image";
 
 import WindowLayout from "@/components/WindowLayout";
 
@@ -6,7 +7,17 @@ const Home: NextPage = () => {
 	return (
 		<div className="w-screen h-screen flex items-center justify-center p-5 bg-blue-500">
 			<WindowLayout>
-				<h1>Hello, World!</h1>
+				<div className="flex flex-col gap-2 items-center justify-center w-full h-full">
+					<Image
+						src="/avatar.jpeg"
+						alt="Tiago Ryan Avatar"
+						width={150}
+						height={150}
+						className="rounded-xl mb-5"
+					/>
+					<h1 className="text-center text-4xl text-zinc-100 font-bold">Tiago Ryan</h1>
+					<h2 className="text-center text-xl text-green-500">FullStack Developer</h2>
+				</div>
 			</WindowLayout>
 		</div>
 	);
