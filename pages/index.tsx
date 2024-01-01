e7dca5e1e7c710072854a07990145d424d2045ef
tiagoryandev <tiagoryandev@gmail.com>
2024-01-01 14:31:04 -0300

    style: remove padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import WindowLayout from "@components/WindowLayout";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen overflow-hidden p-5">
+    <div className="h-screen w-screen overflow-hidden">
       <WindowLayout>teste</WindowLayout>
     </div>
   );
