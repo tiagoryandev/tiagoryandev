1756abba0044e1b18d87adfb13a91d572777afdd
tiagoryandev <tiagoryandev@gmail.com>
2024-01-03 00:03:30 -0300

    feat: add aboutme page

---
+++ pages/404.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { SiVisualstudiocode } from "react-icons/si";
+
+export default function NotFound() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="flex flex-col items-center gap-5">
+        <SiVisualstudiocode className="text-9xl text-[#111111]" />
+        <div className="flex flex-col items-center gap-2">
+          <p className="select-none text-center text-zinc-400">
+            Página não encontrada!
+          </p>
+          <div className="flex items-center gap-2">
+            <span className="select-none text-xs text-zinc-400">
+              Ir para o Início
+            </span>
+            <Link
+              href="/"
+              className="cursor-pointer select-none rounded-[3px] bg-neutral-800 p-[2px_3px] text-xs font-light text-zinc-400 outline-none hover:bg-neutral-700 hover:text-zinc-300"
+            >
+              Home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
