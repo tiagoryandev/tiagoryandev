95e2cb81b594f3eefb36ead1d0dfb537ec40a855
tiagoryandev <tiagoryandev@gmail.com>
2024-01-03 00:13:08 -0300

    feat: add navigation in sidebar window

--- components/WindowLayout/WindowSidebar.tsx
+++ components/WindowLayout/WindowSidebar.tsx
@@ -1,47 +1,45 @@
 import {
   VscFiles,
-  VscSearch,
+  VscBook,
   VscSourceControl,
-  VscDebugAlt,
-  VscExtensions,
   VscGear,
   VscAccount,
 } from "react-icons/vsc";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function WindowSidebar() {
+  const router = useRouter();
+
   return (
     <div className="grid grid-rows-[1fr_min-content] content-between border-l-[1px] border-neutral-700 bg-neutral-800">
       <div className="overflow-hidden">
-        <div
-          data-selected="true"
-          className="group cursor-pointer border-l-2 border-l-transparent p-3 data-[selected=true]:border-l-zinc-200"
-        >
-          <VscFiles className="text-2xl text-zinc-500 group-hover:text-zinc-100 group-data-[selected=true]:text-zinc-100" />
-        </div>
-        <div
-          data-selected="false"
-          className="group cursor-pointer border-l-2 border-l-transparent p-3 data-[selected=true]:border-l-zinc-200"
-        >
-          <VscSearch className="text-2xl text-zinc-500 group-hover:text-zinc-100 group-data-[selected=true]:text-zinc-100" />
-        </div>
-        <div
-          data-selected="false"
-          className="group cursor-pointer border-l-2 border-l-transparent p-3 data-[selected=true]:border-l-zinc-200"
-        >
-          <VscSourceControl className="text-2xl text-zinc-500 group-hover:text-zinc-100 group-data-[selected=true]:text-zinc-100" />
-        </div>
-        <div
-          data-selected="false"
-          className="group cursor-pointer border-l-2 border-l-transparent p-3 data-[selected=true]:border-l-zinc-200"
-        >
-          <VscDebugAlt className="text-2xl text-zinc-500 group-hover:text-zinc-100 group-data-[selected=true]:text-zinc-100" />
-        </div>
-        <div
-          data-selected="false"
-          className="group cursor-pointer border-l-2 border-l-transparent p-3 data-[selected=true]:border-l-zinc-200"
-        >
-          <VscExtensions className="text-2xl text-zinc-500 group-hover:text-zinc-100" />
-        </div>
+        <Link href="/">
+          <div
+            data-selected={router.pathname == "/"}
+            className="group cursor-pointer border-l-2 border-l-transparent p-3 data-[selected=true]:border-l-zinc-200"
+          >
+            <VscFiles className="text-2xl text-zinc-500 group-hover:text-zinc-100 group-data-[selected=true]:text-zinc-100" />
+          </div>
+        </Link>
+
+        <Link href="/about-me" className="outline-none">
+          <div
+            data-selected={router.pathname == "/about-me"}
+            className="group cursor-pointer border-l-2 border-l-transparent p-3 data-[selected=true]:border-l-zinc-200"
+          >
+            <VscBook className="text-2xl text-zinc-500 group-hover:text-zinc-100 group-data-[selected=true]:text-zinc-100" />
+          </div>
+        </Link>
+
+        <Link href="/commits" className="outline-none">
+          <div
+            data-selected={router.pathname == "/commits"}
+            className="group cursor-pointer border-l-2 border-l-transparent p-3 data-[selected=true]:border-l-zinc-200"
+          >
+            <VscSourceControl className="text-2xl text-zinc-500 group-hover:text-zinc-100 group-data-[selected=true]:text-zinc-100" />
+          </div>
+        </Link>
       </div>
 
       <div className="flex min-h-[min-content] flex-col">
